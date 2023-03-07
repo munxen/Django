4b4b1dfc3ffc74495cdcca2054df991dc5ef2d0d
Orlov Yuriy <orlov0975@bk.ru>
2023-03-07 14:30:18 +0300

    Building URL_adresses in templereses

--- Djsite/coolsite/car/urls.py
+++ Djsite/coolsite/car/urls.py
@@ -3,6 +3,10 @@ from django.urls import path, re_path
 from .views import *
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', index, name='home'),
     path('about/', about, name='about'),
+    path('partners', partners, name='partners'),
+    path('contact/', contact, name='contact'),
+    path('login/', login, name='login'),
+    path('post/<int:post_id>/', show_post, name='post'),
 ]
--- Djsite/coolsite/car/views.py
+++ Djsite/coolsite/car/views.py
@@ -3,25 +3,32 @@ from django.http import HttpResponse, HttpResponseNotFound, Http404
 
 from .models  import *
 
-menu = ["О сайте", "Партнёрам", "Обратная связь", "Войти"]
-
+menu = [{'title': "О сайте", 'url_name': 'about'},
+        {'title': "Партнёрам", 'url_name': 'partners'},
+        {'title': "Обратная связь", 'url_name': 'contact'},
+        {'title': "Войти", 'url_name': 'login'}]
 # Create your views here.
 def index(request):
     posts = Car.objects.all()
-    return render(request, 'car/index.html', {'posts':posts , 'menu':menu, 'title': 'Главная страница'})
+    context = {'posts': posts,
+               'menu': menu,
+               'title': 'Главная страница'}
+    return render(request, 'car/index.html',context = context)
 
 def about(request):
     return render(request, 'car/about.html', {'menu':menu, 'title':'О сайте'}) 
 
-def categories(request, carid):
-    print (request.GET)
-    return HttpResponse(f"<h1>Категории<h1><p>{carid}<p>")
-
-def archive(request, year):
-    if int(year) > 2023:
-        return redirect('home', permanent=True)
-    
-    return HttpResponse(f"<h1>Архив по годам<h1><p>{year}<p>")
-
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
+
+def partners(request):
+    return HttpResponse ("Партнёрам")
+
+def contact(request):
+    return HttpResponse ("Обратная связь")
+
+def login(request):
+    return HttpResponse ("Авторизация")
+
+def show_post(request, post_id):
+    return HttpResponse (f"Отображение статьи с id = {post_id}")
